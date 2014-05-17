02e72753cdcc0e2c19f7937d2552b9c753a7a66c
shinnnosuke1232 <shinnnosuke1232@yahoo.co.jp>
2014-05-17 11:49:39 +0900

    カッコの書き換え

--- a.c
+++ a.c
@@ -10,32 +10,32 @@ int main()
 	printf("<h1>99</h1>\n");
 
 	printf("<table>\n");
-	for(i = 0; i < 10; ++i)
-	{
+	for(i = 0; i < 10; ++i){
+
 		printf("<tr>\n");
-		for(j = 0; j < 10; ++j)
-		{
-			if(j == 0)
-			{
-				if( i == 0)
-				{
+		for(j = 0; j < 10; ++j){
+
+			if(j == 0){
+
+				if( i == 0){
+
 					printf("<th>\n");
 				}
-				else if(i != 0)
-				{
+				else if(i != 0){
+
 					printf("<th>%d\n",i);
 				}
 			}
-			else if( i == 0)
-			{
+			else if( i == 0){
+
 				printf("<th>%d\n", j);
 			}
-			else if( j * i == 0)
-			{
+			else if( j * i == 0){
+
 				printf("<th>%d\n", j);
 			}
-			else
-			{
+			else{
+
 				printf("<td>%d\n", j * i);
 			}
 		}
